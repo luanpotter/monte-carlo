8c197487f1776bb2879a0956cc24464feff22924
Luan Nico <luanpotter27@gmail.com>
2018-10-03 20:01:43 -0300

    custom ranges

--- tasks/task04.py
+++ tasks/task04.py
@@ -3,23 +3,26 @@
 import random
 import math
 
-IT = 10**7
+IT = 10**6
 
-def did_hit(fn):
-  x = random.random()
-  y = random.random()
+def did_hit(fn, a, b, maxy):
+  x = a + (b - a) * random.random()
+  y = maxy * random.random()
 
   ty = fn(x)
   return y <= ty
 
-def mc(fn):
+def mc(fn, a = 0, b = 1, maxy = None):
+  maxy = maxy or fn(b)
+  rectArea = (b - a) * maxy
   hits = 0
-  for _ in range(0, IT + 1):
-    if (did_hit(fn)):
+  for _ in range(0, IT):
+    if (did_hit(fn, a, b, maxy)):
       hits = hits + 1
+  intArea = rectArea * (hits / IT)
   print('Hits %s' % hits)
   print('Total %s' % IT)
-  print('Area %s' % (hits / IT))
+  print('Area %s' % intArea)
 
 fn = lambda x: x**2
-mc(fn)
+mc(fn, 2, 3)
