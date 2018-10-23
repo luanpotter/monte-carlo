8ffb304fd7d3f240b5882278940d8d93e7aaace1
Luan Nico <luanpotter27@gmail.com>
2018-10-22 23:07:12 -0300

    Adding render, improving rocks

--- part2/rocks.py
+++ part2/rocks.py
@@ -5,11 +5,8 @@ import math
 
 IT = 10**6
 
-def r():
-    return random.random()
-
-def did_hit(x, y):
-    return x**2 + y**2 < 1
+def r(): return random.random()
+def did_hit(x, y): return x**2 + y**2 < 1
 
 def run(it):
     hits = 0
@@ -18,11 +15,16 @@ def run(it):
         if (did_hit(x, y)): hits += 1
     return hits
 
-def main():
-    hits = run(IT)
-    p = hits / IT
+def main(it):
+    hits = run(it)
+    p = hits / it
     pi = 4 * p
-    print(pi)
+    return pi
 
-main()
+def m():
+    print('E1 : %s' % main(10**1))
+    print('E4 : %s' % main(10**4))
+    print('E6 : %s' % main(10**6))
+    print('E9 : %s' % main(10**9))
 
+m()
