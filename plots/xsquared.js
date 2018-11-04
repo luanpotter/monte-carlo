82bb89b94d2586f0b8e42a0e93ad0811b9df1d07
Luan Nico <luanpotter27@gmail.com>
2018-11-04 17:05:57 -0200

    adding plots for mono

---
+++ plots/xsquared.js
@@ -0,0 +1,23 @@
+const { build, plot } = require('./plotly.js');
+
+const xs = Array(1000).fill().map((_, i) => i / 1000);
+const g = x => x * x;
+
+const pt = build();
+plot(pt, {
+	data: xs,
+	title: 'x',
+}, {
+	data: xs.map(g),
+	title: 'g(x)',
+}, 'g_x', () => console.log('g done'));
+
+const f = x => 1;
+
+plot(pt, {
+	data: xs,
+	title: 'u',
+}, {
+	data: xs.map(f),
+	title: 'f(u)',
+}, 'f_u', () => console.log('f done'));
