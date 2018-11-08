56aae1f01e3bb40a0b6f35e398306987fbde8b0e
Luan Nico <luanpotter27@gmail.com>
2018-11-08 15:31:12 -0200

    .

--- part1/task03.py
+++ part1/task03.py
@@ -6,18 +6,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 MAX = 10**5
-BINS = 20
+BINS = 50
 f = lambda x: -2.2*math.log(1 - x)
 
 def plot(values):
   plt.hist(values, histtype='bar', rwidth=0.8, color='black', bins=BINS)
+  plt.axvline(x=2.2)
   plt.xlabel('Tempo de Decaimento ($\\mu s$)')
   plt.ylabel('Frequência')
   plt.title('Histograma dos Tempos de Decaimento para $\\tau = 2.2 \\mu s$')
 
 def run(fn):
-  numbers = list(map(fn, map(lambda x: random.random(), range(1, MAX))))
+  numbers = list(map(fn, map(lambda x: random.random(), range(MAX))))
   avg = sum(numbers) / len(numbers)
+  print(avg)
   plot(numbers)
 
 run(f)
