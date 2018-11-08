4478456045ff128659518b66e14bec19c307388e
Luan Nico <luanpotter27@gmail.com>
2018-11-08 15:10:26 -0200

    moar bins

--- part1/task03.py
+++ part1/task03.py
@@ -6,10 +6,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 MAX = 10**5
+BINS = 20
 f = lambda x: -2.2*math.log(1 - x)
 
 def plot(values):
-  plt.hist(values, histtype='bar', rwidth=0.8, color='black')
+  plt.hist(values, histtype='bar', rwidth=0.8, color='black', bins=BINS)
   plt.xlabel('Tempo de Decaimento ($\\mu s$)')
   plt.ylabel('Frequência')
   plt.title('Histograma dos Tempos de Decaimento para $\\tau = 2.2 \\mu s$')
