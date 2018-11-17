eee8dd13aa8eefd9f09be1453af3c3b952b5df93
Luan Nico <luanpotter27@gmail.com>
2018-11-17 20:34:27 -0200

    .

---
+++ exposicao/theta_and_phi.py
@@ -0,0 +1,38 @@
+#!/bin/python
+
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+MAX = 10**6
+
+ctheta = lambda x: 2*x - 1
+theta = lambda x: math.acos(ctheta(x))
+phi = lambda x: 2 * math.pi * x
+
+run = lambda fn: list(map(fn, map(lambda x: random.random(), range(MAX))))
+
+n = MAX / 10
+
+thetas = run(theta)
+plt.hist(thetas, color='grey')
+plt.plot([0, math.pi], [n, n], color='k')
+plt.ylabel('Frequência')
+plt.xlabel('$\\theta$')
+
+plt.tight_layout(0.5)
+plt.savefig('theta.png')
+
+plt.clf()
+
+phis = run(phi)
+plt.hist(phis, color='grey')
+plt.plot([0, 2*math.pi], [n, n], color='k')
+plt.ylabel('Frequência')
+plt.xlabel('$\\phi$')
+
+plt.tight_layout(0.5)
+plt.savefig('phi.png')
+
+exit()
