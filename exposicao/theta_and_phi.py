41415e4284146b2a18a0881b6c0a5e47ce7c2e1c
Luan Nico <luanpotter27@gmail.com>
2018-11-17 20:39:12 -0200

    .

--- exposicao/theta_and_phi.py
+++ exposicao/theta_and_phi.py
@@ -6,18 +6,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 MAX = 10**6
+BINS = 10
 
-ctheta = lambda x: 2*x - 1
-theta = lambda x: math.acos(ctheta(x))
+r = lambda: random.random()
+lmap = lambda arr, fn: list(map(fn, arr))
+
+theta = lambda x: math.acos(2*x - 1)
 phi = lambda x: 2 * math.pi * x
 
-run = lambda fn: list(map(fn, map(lambda x: random.random(), range(MAX))))
+run = lambda fn: lmap(lmap(range(MAX), lambda x: r()), fn)
 
-n = MAX / 10
+n = MAX / BINS # normalization factor
 
 thetas = run(theta)
-plt.hist(thetas, color='grey')
-plt.plot([0, math.pi], [n, n], color='k')
+plt.hist(thetas, color='grey', bins=BINS)
+plt.plot([0, math.pi], [n, n], color='k') # this would be an ideal uniform pdf
 plt.ylabel('Frequência')
 plt.xlabel('$\\theta$')
 
@@ -27,12 +30,10 @@ plt.savefig('theta.png')
 plt.clf()
 
 phis = run(phi)
-plt.hist(phis, color='grey')
-plt.plot([0, 2*math.pi], [n, n], color='k')
+plt.hist(phis, color='grey', bins=BINS)
+plt.plot([0, 2*math.pi], [n, n], color='k') # this would be an ideal uniform pdf
 plt.ylabel('Frequência')
 plt.xlabel('$\\phi$')
 
 plt.tight_layout(0.5)
 plt.savefig('phi.png')
-
-exit()
